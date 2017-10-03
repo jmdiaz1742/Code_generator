de74a10b2ed159e29cb61ccf02ac07540be2fbee
HONEYWELL\H112943 <jose.diaz@honeywell.com>
2017-10-03 17:46:01 -0600

    Updated file choosing

--- Code_generator/src/gui/MainWindow.java
+++ Code_generator/src/gui/MainWindow.java
@@ -7,20 +7,20 @@ import javax.swing.JFrame;
 import common.Features;
 import configurator.ConfigurationFile;
 import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import java.awt.Insets;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JFileChooser;
 import javax.swing.JMenu;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 /**
  * Main application window
@@ -95,54 +95,82 @@ public class MainWindow {
 		mnFile.add(mntmNewProject);
 		
 		JMenuItem mntmOpenProject = new JMenuItem(Messages.getString("MainWindow.mntmOpenProject.text")); //$NON-NLS-1$
+		mntmOpenProject.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/****** Begin Open Existing Project button click ******/
+				getProjectFile();
+				/****** End Open Existing Project button click ******/
+			}
+		});
 		mnFile.add(mntmOpenProject);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		FrmCodeGenerator.getContentPane().setLayout(gridBagLayout);
 		
-		JButton btnNewProject = new JButton(Messages.getString("MainWindow.btnNewProject.text")); //$NON-NLS-1$
-		GridBagConstraints gbc_btnNewProject = new GridBagConstraints();
-		gbc_btnNewProject.anchor = GridBagConstraints.WEST;
-		gbc_btnNewProject.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewProject.gridx = 0;
-		gbc_btnNewProject.gridy = 0;
-		FrmCodeGenerator.getContentPane().add(btnNewProject, gbc_btnNewProject);
+		JLabel lblt_ProjectName = new JLabel(Messages.getString("MainWindow.lblProject.text")); //$NON-NLS-1$
+		GridBagConstraints gbc_lblt_ProjectName = new GridBagConstraints();
+		gbc_lblt_ProjectName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblt_ProjectName.gridx = 0;
+		gbc_lblt_ProjectName.gridy = 0;
+		FrmCodeGenerator.getContentPane().add(lblt_ProjectName, gbc_lblt_ProjectName);
 		
-		JButton btnOpenExistingProject = new JButton(Messages.getString("MainWindow.btnOpenExistingProject.text")); //$NON-NLS-1$
-		btnOpenExistingProject.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				FileNameExtensionFilter projectFileFilter;
-				final JFileChooser ProjectFileChooser;
-				
-				String initialSearchPath = System.getProperty("user.dir");
-				if (Features.DEBUG) {
-					/* Use the example project when debugging */
-					initialSearchPath += System.getProperty("file.separator") + "testProject";
-				}
-				
-				ProjectFileChooser = new JFileChooser(initialSearchPath);
-				projectFileFilter = new FileNameExtensionFilter("Project configuration files", ConfigurationFile.STR_PROJ_CONF_FILE);
-				
-				ProjectFileChooser.setFileFilter(projectFileFilter);
-				int fileOpenError = ProjectFileChooser.showOpenDialog(FrmCodeGenerator);
-				if (fileOpenError == JFileChooser.APPROVE_OPTION) {
-					/* Load the configuration file */
-					MainGui.loadProjectFile(ProjectFileChooser.getSelectedFile());
-				} else {
-					Features.verbosePrint("Error opening project configuration file...");
-				}
-			}
-		});
-		GridBagConstraints gbc_btnOpenExistingProject = new GridBagConstraints();
-		gbc_btnOpenExistingProject.gridx = 0;
-		gbc_btnOpenExistingProject.gridy = 1;
-		FrmCodeGenerator.getContentPane().add(btnOpenExistingProject, gbc_btnOpenExistingProject);
+		JLabel lbl_ProjectName = new JLabel(Messages.getString("MainWindow.lblNewLabel.text")); //$NON-NLS-1$
+		GridBagConstraints gbc_lbl_ProjectName = new GridBagConstraints();
+		gbc_lbl_ProjectName.insets = new Insets(0, 0, 5, 0);
+		gbc_lbl_ProjectName.gridx = 1;
+		gbc_lbl_ProjectName.gridy = 0;
+		FrmCodeGenerator.getContentPane().add(lbl_ProjectName, gbc_lbl_ProjectName);
 	}
 	
+	/**
+	 * Call the file chooser to get the project configuration file
+	 */
+	private void getProjectFile() {
+		FileNameExtensionFilter projectFileFilter;
+		String initialSearchPath = System.getProperty("user.dir");
+		File inFile;
+		
+		if (Features.DEBUG) {
+			/* Use the example project when debugging */
+			initialSearchPath += System.getProperty("file.separator") + "testProject";
+		}
+		
+		projectFileFilter = new FileNameExtensionFilter(Messages.getString("MainWindow.projectFileFilter.text"), ConfigurationFile.STR_PROJ_CONF_FILE);
+		inFile = OpenFileChooser(initialSearchPath, Messages.getString("MainWindow.projectFileChooser.title"), projectFileFilter);
+		if (null != inFile) {
+			MainGui.loadProjectFile(inFile);
+		}
+	}
 	
-
+	/**
+	 * Open file chooser dialog and get the selected file
+	 * @param initialPath Path to search the file in
+	 * @param title Dialog title
+	 * @param fileFilter Extension filter
+	 * @return Selected file
+	 */
+	public File OpenFileChooser(String initialPath, String title, FileNameExtensionFilter fileFilter) {
+		File inFile = null;
+		final JFileChooser ProjectFileChooser = new JFileChooser(initialPath);
+		
+		ProjectFileChooser.setDialogTitle(title);
+		ProjectFileChooser.setFileFilter(fileFilter);
+		int fileOpenError = ProjectFileChooser.showOpenDialog(FrmCodeGenerator);
+		if (fileOpenError == JFileChooser.APPROVE_OPTION) {
+			/* File selected */
+			inFile = ProjectFileChooser.getSelectedFile();
+			Features.verbosePrint("File " + inFile.getName() + " selected...");
+		} else {
+			/* Operation canceled */
+			Features.verbosePrint("No file selected...");
+		}
+		
+		return inFile;
+	}
+	
+	public void setprojectName
 
 }
