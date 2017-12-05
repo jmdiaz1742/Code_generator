6abc0122aeb616231556b463cf163854005f1215
HONEYWELL\H112943 <jose.diaz@honeywell.com>
2017-12-04 17:31:50 -0700

    Correcting code generation scripts

--- Code_generator/generateDoc.sh
+++ Code_generator/generateDoc.sh
@@ -1,4 +1,3 @@
-
 TITLE="Generate doxygen documentation"
 
 # The first parameter is the Doxygen file name
@@ -7,19 +6,19 @@ fileName="$1"
 # Check input parameters
 if [ -z $fileName ]
 then
-    echo "No file name specified, using \"Doxyfile\"..."
+    echo "No file name specified, using Doxyfile..."
     fileName="Doxyfile"
 fi
 
 # Generate HTML documentation
 echo "Using Doxygen..."
-doxygen Doxyfile \
+doxygen $fileName \
 && (echo "HTML documentation completed...") \
-|| (echo "Problem vuilding HTML documentation!"; exit 1)
+|| (echo "Problem building HTML documentation!"; exit 1)
 
 # Generate PDF documentation
-cd doc/latex
 echo Generating pdf...
+cd doc/latex
 make \
 && (echo "PDF documentation completed...") \
-|| (echo "Problem vuilding PDF documentation!"; exit 1)
+|| (echo "Problem building PDF documentation!"; exit 1)
