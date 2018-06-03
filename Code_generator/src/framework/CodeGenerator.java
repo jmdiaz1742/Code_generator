eaa92caa1a1fc9276a83a83c98aa6de047f6d9e2
jmdiaz1742 <jmdiaz1742@gmail.com>
2018-06-02 23:24:18 -0600

    Restructured Code generation
    - Generic code generation methods are now in the Code generation class
    
    - Automatic code beautifying is not working yet, Linux command doesn't
    execute correctly

--- Code_generator/src/framework/CodeGenerator.java
+++ Code_generator/src/framework/CodeGenerator.java
@@ -1,6 +1,19 @@
 package framework;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 import common.ErrorCode;
+import common.Features;
 import microcontroller.Microcontroller;
 import projectConfiguration.ProjectSettings;
 
@@ -11,8 +24,10 @@ import projectConfiguration.ProjectSettings;
  */
 public class CodeGenerator {
 
-	private Microcontroller SelectedUc;
-	private ProjectSettings CurrentProjectSettings;
+	private Microcontroller uC;
+	private ProjectSettings projectSettings;
+
+	private static final String MODULE_GPIO = "gpio";
 
 	/**
 	 * Constructor
@@ -23,8 +38,8 @@ public class CodeGenerator {
 	 *            Project's settings
 	 */
 	public CodeGenerator(Microcontroller uC, ProjectSettings projectSettings) {
-		SelectedUc = uC;
-		CurrentProjectSettings = projectSettings;
+		this.uC = uC;
+		this.projectSettings = projectSettings;
 	}
 
 	/**
@@ -36,10 +51,208 @@ public class CodeGenerator {
 		ErrorCode error = ErrorCode.NO_ERROR;
 
 		// Generate GPIO module files:
-		error = GpioGenerator.generateCfgCFile(SelectedUc, CurrentProjectSettings);
-		error = GpioGenerator.generateCfgHFile(SelectedUc, CurrentProjectSettings);
+		error = generateCfgFiles(MODULE_GPIO);
 
 		return error;
+	}
+
+	/**
+	 * Generate configuration .c and .h files
+	 * 
+	 * @param module
+	 *            Framework module
+	 * @return Error Code
+	 */
+	private ErrorCode generateCfgFiles(String module) {
+		ErrorCode error = ErrorCode.NO_ERROR;
+		File cfgFile = null;
+		File fwkFile = null;
+
+		Features.verbosePrint("Generating " + module + " configuration C file...");
+
+		fwkFile = new File(framework.Common.getCfgFileCPath(framework.Common.getInstallationFwkPath(), module));
+		cfgFile = new File(framework.Common.getCfgFileCPath(projectSettings.getFrameworkPath(), module));
+
+		error = copyFile(fwkFile, cfgFile);
+
+		switch (module) {
+		case MODULE_GPIO: {
+			error = replaceInFile(cfgFile, GpioGenerator.STR_TKN_CFG_ARRAY, GpioGenerator.getCfgArray(uC));
+			break;
+		}
+		default: {
+			error = ErrorCode.FILE_CONF_ERROR;
+			break;
+		}
+		}
+		beautifyFile(cfgFile);
+
+		Features.verbosePrint("Generating " + module + " configuration H file...");
+
+		fwkFile = new File(framework.Common.getCfgFileHPath(framework.Common.getInstallationFwkPath(), module));
+		cfgFile = new File(framework.Common.getCfgFileHPath(projectSettings.getFrameworkPath(), module));
+
+		error = copyFile(fwkFile, cfgFile);
+
+		switch (module) {
+		case MODULE_GPIO: {
+			replaceInFile(cfgFile, GpioGenerator.STR_TKN_EL_DEFS, GpioGenerator.getElDefs(uC));
+			replaceInFile(cfgFile, GpioGenerator.STR_TKN_ELEMENTS, GpioGenerator.getElements(uC));
+			break;
+		}
+		default: {
+			error = ErrorCode.FILE_CONF_ERROR;
+			break;
+		}
+		}
+		beautifyFile(cfgFile);
+
+		return error;
+	}
+
+	/**
+	 * Copies one file from one location to another
+	 * 
+	 * @param src
+	 *            Original file
+	 * @param dest
+	 *            New file
+	 * @return Error Code
+	 */
+	static private ErrorCode copyFile(File src, File dest) {
+		ErrorCode error = ErrorCode.NO_ERROR;
+		InputStream inFile = null;
+		OutputStream outFile = null;
+
+		try {
+			// Copy template file from installation folder
+			inFile = new FileInputStream(src);
+			outFile = new FileOutputStream(dest);
+
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inFile.read(buffer)) > 0) {
+				outFile.write(buffer, 0, length);
+			}
+
+		} catch (FileNotFoundException e1) {
+			error = ErrorCode.FILE_READ_ERROR;
+			Features.verbosePrint("Error opening file" + src.getPath());
+			e1.printStackTrace();
+			return error;
+		} catch (IOException e) {
+			error = ErrorCode.FILE_WRITE_ERROR;
+			Features.verbosePrint("Error saving file" + dest.getPath());
+			e.printStackTrace();
+			return error;
+		} finally {
+			try {
+				inFile.close();
+				outFile.close();
+			} catch (IOException e) {
+				error = ErrorCode.FILE_WRITE_ERROR;
+				e.printStackTrace();
+				return error;
+			}
+
+		}
+
+		return error;
+	}
+
+	/**
+	 * Replaces text inside a file
+	 * 
+	 * @param file
+	 *            Configuration File
+	 * @param originalStr
+	 *            String to replace
+	 * @param newStf
+	 *            New String
+	 * @return Error Code
+	 */
+	private static ErrorCode replaceInFile(File file, String originalStr, String newStf) {
+		ErrorCode error = ErrorCode.NO_ERROR;
+		String currentFileText = "";
+		String newFileText = "";
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				currentFileText += line + "\r\n";
+			}
+			reader.close();
+
+			// Replace token string on template with configuration array
+			newFileText = currentFileText.replaceAll(originalStr, newStf);
+
+			FileWriter writer = new FileWriter(file.getAbsolutePath());
+			writer.write(newFileText);
+			writer.close();
+
+		} catch (FileNotFoundException e) {
+			error = ErrorCode.FILE_READ_ERROR;
+			e.printStackTrace();
+		} catch (IOException e) {
+			error = ErrorCode.FILE_WRITE_ERROR;
+			e.printStackTrace();
+		}
+
+		return error;
+	}
+
+	/**
+	 * Beautifies a .c or .h file using the .clang_format configuration settings
+	 * 
+	 * @param file
+	 *            File to beautify
+	 */
+	private static void beautifyFile(File file) {
+		// String command = "clang-format -i -style=file " + "\"" + file.getPath() +
+		// "\"";
+		String command = "cd " + "\"" + file.getParent() + "\" && echo clang-format -i -style=file " + file.getName()
+				+ " >> beautify.sh && sh beautify.sh";
+		String consoleText = "";
+		String consoleBuffer = null;
+		Process process;
+
+		if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+			command = "cmd.exe /c" + command;
+		} else if (System.getProperty("os.name").toLowerCase().startsWith("linux")) {
+			Features.verbosePrint("Executing command: " + command);
+			try {
+				process = Runtime.getRuntime().exec(command);
+
+				BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+				BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+				// read the output from the command
+				System.out.println("Here is the standard output of the command:\n");
+				while ((consoleBuffer = stdInput.readLine()) != null) {
+					consoleText += consoleBuffer;
+				}
+				Features.verbosePrint("Console output: " + consoleText);
+
+				consoleText = "";
+				consoleBuffer = null;
+
+				// read any errors from the attempted command
+				while ((consoleBuffer = stdError.readLine()) != null) {
+					consoleText += consoleBuffer;
+				}
+				Features.verbosePrint("Console error: " + consoleText);
+
+				System.exit(0);
+			} catch (IOException e) {
+				Features.verbosePrint("Error executing command " + command);
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		} else {
+			Features.verbosePrint("Error: OS not found!");
+		}
 	}
 
 }
--- Code_generator/src/framework/GpioGenerator.java
+++ Code_generator/src/framework/GpioGenerator.java
@@ -1,26 +1,10 @@
 package framework;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import common.ErrorCode;
-import common.Features;
 import microcontroller.Microcontroller;
-import projectConfiguration.ProjectSettings;
 
 class GpioGenerator {
 
-	private static final String MODULE_GPIO = "gpio";
 	private static final String STR_ALT_MODE_NONE = "GPIO_ALT_NONE";
-	private static final String STR_ALT_MODE_ADC = "GPIO_MODE_ANALOG";
 
 	private static final String STR_SUFF_PORT = "_PORT";
 	private static final String STR_SUFF_PIN = "_PIN";
@@ -30,170 +14,11 @@ class GpioGenerator {
 	private static final String STR_SUFF_SPEED = "_SPEED";
 
 	/* Toke Strings */
-	private final static String STR_TKN_CFG_ARRAY = "FWK_GPIO_CFG_ARRAY";
-	private final static String STR_TKN_ELEMENTS = "FWK_GPIO_ELEMENTS";
-	private final static String STR_TKN_INC = "FWK_GPIO_INCLUDES"; // TODO: Get correct use
-	private final static String STR_TKN_CFG_DEFS = "FWK_GPIO_CFG_DEFINITIONS"; // TODO: Get correct use
-	private final static String STR_TKN_EL_DEFS = "FWK_GPIO_ELEMENTS_DEFINITIONS";
-
-	/**
-	 * 
-	 * @param uC
-	 *            Microcontroller
-	 * @param projectSettings
-	 *            Project's settings
-	 * @return Error code
-	 */
-	public static ErrorCode generateCfgCFile(Microcontroller uC, ProjectSettings projectSettings) {
-		ErrorCode error = ErrorCode.NO_ERROR;
-		File cfgFile = null;
-		File fwkFile = null;
-		String currentFileText = "";
-		String newFileText = "";
-		InputStream inFile = null;
-		OutputStream outFile = null;
-
-		try {
-			// Copy template file from installation folder
-			fwkFile = new File(
-					framework.Common.getCfgFileCPath(framework.Common.getInstallationFwkPath(), MODULE_GPIO));
-			cfgFile = new File(framework.Common.getCfgFileCPath(projectSettings.getFrameworkPath(), MODULE_GPIO));
-			inFile = new FileInputStream(fwkFile);
-			outFile = new FileOutputStream(cfgFile);
-
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = inFile.read(buffer)) > 0) {
-				outFile.write(buffer, 0, length);
-			}
-
-		} catch (FileNotFoundException e1) {
-			error = ErrorCode.FILE_READ_ERROR;
-			Features.verbosePrint("Error opening file"
-					+ framework.Common.getCfgFileCPath(projectSettings.getFrameworkPath(), MODULE_GPIO));
-			e1.printStackTrace();
-			return error;
-		} catch (IOException e) {
-			error = ErrorCode.FILE_WRITE_ERROR;
-			e.printStackTrace();
-			return error;
-		} finally {
-			try {
-				inFile.close();
-				outFile.close();
-			} catch (IOException e) {
-				error = ErrorCode.FILE_WRITE_ERROR;
-				e.printStackTrace();
-				return error;
-			}
-
-		}
-
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(cfgFile));
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				currentFileText += line + "\r\n";
-			}
-			reader.close();
-
-			// Replace token string on template with configuration array
-			newFileText = currentFileText.replaceAll(STR_TKN_CFG_ARRAY, generateCfgArray(uC));
-
-			FileWriter writer = new FileWriter(cfgFile.getAbsolutePath());
-			writer.write(newFileText);
-			writer.close();
-
-		} catch (FileNotFoundException e) {
-			error = ErrorCode.FILE_READ_ERROR;
-			e.printStackTrace();
-		} catch (IOException e) {
-			error = ErrorCode.FILE_WRITE_ERROR;
-			e.printStackTrace();
-		}
-
-		return error;
-	}
-
-	/**
-	 * 
-	 * @param uC
-	 *            Microcontroller
-	 * @param projectSettings
-	 *            Project's settings
-	 * @return Error code
-	 */
-	public static ErrorCode generateCfgHFile(Microcontroller uC, ProjectSettings projectSettings) {
-		ErrorCode error = ErrorCode.NO_ERROR;
-		File cfgFile = null;
-		File fwkFile = null;
-		String currentFileText = "";
-		String newFileText = "";
-		InputStream inFile = null;
-		OutputStream outFile = null;
-
-		try {
-			// Copy template file from installation folder
-			fwkFile = new File(
-					framework.Common.getCfgFileHPath(framework.Common.getInstallationFwkPath(), MODULE_GPIO));
-			cfgFile = new File(framework.Common.getCfgFileHPath(projectSettings.getFrameworkPath(), MODULE_GPIO));
-			inFile = new FileInputStream(fwkFile);
-			outFile = new FileOutputStream(cfgFile);
-
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = inFile.read(buffer)) > 0) {
-				outFile.write(buffer, 0, length);
-			}
-
-		} catch (FileNotFoundException e1) {
-			error = ErrorCode.FILE_READ_ERROR;
-			Features.verbosePrint("Error opening file"
-					+ framework.Common.getCfgFileCPath(projectSettings.getFrameworkPath(), MODULE_GPIO));
-			e1.printStackTrace();
-			return error;
-		} catch (IOException e) {
-			error = ErrorCode.FILE_WRITE_ERROR;
-			e.printStackTrace();
-			return error;
-		} finally {
-			try {
-				inFile.close();
-				outFile.close();
-			} catch (IOException e) {
-				error = ErrorCode.FILE_WRITE_ERROR;
-				e.printStackTrace();
-				return error;
-			}
-
-		}
-
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(cfgFile));
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				currentFileText += line + "\r\n";
-			}
-			reader.close();
-
-			// Replace token string on template with configuration array
-			newFileText = currentFileText.replaceAll(STR_TKN_EL_DEFS, generateElDefs(uC));
-			newFileText = newFileText.replaceAll(STR_TKN_ELEMENTS, generateElements(uC));
-
-			FileWriter writer = new FileWriter(cfgFile.getAbsolutePath());
-			writer.write(newFileText);
-			writer.close();
-
-		} catch (FileNotFoundException e) {
-			error = ErrorCode.FILE_READ_ERROR;
-			e.printStackTrace();
-		} catch (IOException e) {
-			error = ErrorCode.FILE_WRITE_ERROR;
-			e.printStackTrace();
-		}
-
-		return error;
-	}
+	public static final String STR_TKN_CFG_ARRAY = "FWK_GPIO_CFG_ARRAY";
+	public static final String STR_TKN_ELEMENTS = "FWK_GPIO_ELEMENTS";
+	public static final String STR_TKN_INC = "FWK_GPIO_INCLUDES"; // TODO: Get correct use
+	public static final String STR_TKN_CFG_DEFS = "FWK_GPIO_CFG_DEFINITIONS"; // TODO: Get correct use
+	public static final String STR_TKN_EL_DEFS = "FWK_GPIO_ELEMENTS_DEFINITIONS";
 
 	/**
 	 * Generate configuration array from microcontroller
@@ -202,7 +27,7 @@ class GpioGenerator {
 	 *            Framework module
 	 * @return Configuration array as string
 	 */
-	private static String generateCfgArray(Microcontroller uC) {
+	public static String getCfgArray(Microcontroller uC) {
 		String cfgArray = "";
 
 		for (int pinNum = 0; pinNum < uC.GpioCfgPin.length; pinNum++) {
@@ -231,7 +56,7 @@ class GpioGenerator {
 	 *            Framework module
 	 * @return Elements definitions as String
 	 */
-	private static String generateElDefs(Microcontroller uC) {
+	public static String getElDefs(Microcontroller uC) {
 		String elDefs = "";
 
 		for (int pinNum = 0; pinNum < uC.GpioCfgPin.length; pinNum++) {
@@ -270,7 +95,7 @@ class GpioGenerator {
 	 *            Framework module
 	 * @return Elements list as String
 	 */
-	private static String generateElements(Microcontroller uC) {
+	public static String getElements(Microcontroller uC) {
 		String elements = "";
 
 		for (int pinNum = 0; pinNum < uC.GpioCfgPin.length; pinNum++) {
