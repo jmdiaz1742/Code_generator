4f3a721a182402834604fdf3efdd9864529e33c9
cesaresq20 <cesaresq20@gmail.com>
2020-06-07 12:27:06 -0600

    - Adding support for ADC in ATMEGA4809 Test Project

--- Code_generator/testProjectATMEGA4809/Src/framework/drivers/adc/cfg/adc_cfg.h
+++ Code_generator/testProjectATMEGA4809/Src/framework/drivers/adc/cfg/adc_cfg.h
@@ -1,21 +1,21 @@
 /**
- * @file gpio_cfg.h
- * @author Miguel Diaz
- * @brief GPIO framework configuration header
+ * @file adc_cfg.h
+ * @author Cesar Rodriguez
+ * @brief ADC framework configuration header
  */
 
 /************************
  * Guard                *
  ************************/
-#ifndef _GPIO_CFG_H_
-#define _GPIO_CFG_H_
+#ifndef _ADC_CFG_H_
+#define _ADC_CFG_H_
 
 /************************
  * Includes             *
  ************************/
 // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this header and the footer below #######
-#include "port.h"
+
 // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this footer and the header above #######
 
@@ -29,13 +29,11 @@ typedef enum
 {
     // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this header and the footer below #######
-USER_BUTTON,
-USER_LED,
-POT,
+ADC1,
 // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this footer and the header above #######
-    GPIO_ELEMENTS_MAX
-} Gpio_elementsType;
+    ADC_ELEMENTS_MAX
+} Adc_elementsType;
 
 /************************
  * Public Macros        *
@@ -52,35 +50,18 @@ POT,
 
 // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this header and the footer below #######
-// USER_BUTTON
-#define USER_BUTTON_PORT PORT_B
-#define USER_BUTTON_PIN PIN_2
-#define USER_BUTTON_MODE MODE_INPUT
-#define USER_BUTTON_ALT_MODE ALT_MODE_NONE
-#define USER_BUTTON_OUT_TYPE OTYPE_PUSH_PULL
-#define USER_BUTTON_PULL PULL_UP
-#define USER_BUTTON_SPEED SPEED_FAST
-#define USER_BUTTON_INIT_OUT LOW
+// ADC1
+#define ADC1_NAME ADC1
+#define ADC1_SAMPLE ADC_DISABLE_SAMPLE
+#define ADC1_CLOCK ADC_NOT_SUPPORTED
+#define ADC1_JUSTIFICATION ADC_NOT_SUPPORTED
+#define ADC1_PRESCALER DIVIDE_ADC_CLOCK_BY_4
+#define ADC1_RESOLUTION ADC_10_BIT_RESOLUTION
+#define ADC1_REFERENCE ADC_USE_SUPPLY_VOLTAGE_AS_REFERENCE
 
-// USER_LED
-#define USER_LED_PORT PORT_B
-#define USER_LED_PIN PIN_5
-#define USER_LED_MODE MODE_OUTPUT
-#define USER_LED_ALT_MODE ALT_MODE_NONE
-#define USER_LED_OUT_TYPE OTYPE_PUSH_PULL
-#define USER_LED_PULL PULL_NOT_AVAILABLE
-#define USER_LED_SPEED SPEED_FAST
-#define USER_LED_INIT_OUT LOW
-
-// POT
-#define POT_PORT PORT_D
-#define POT_PIN PIN_0
-#define POT_MODE MODE_ALTERNATE_FUNCTION
-#define POT_ALT_MODE ALT_MODE_NONE
-#define POT_OUT_TYPE OTYPE_PUSH_PULL
-#define POT_PULL PULL_NOT_AVAILABLE
-#define POT_SPEED SPEED_FAST
-#define POT_INIT_OUT LOW
+// Channel definitions
+// ADC ADC1
+#define POT_ADC_CHANNEL ADC_CHANNEL_0
 
 // ################## Kamino generator v1.1.0: Generated code! ################
 // ######## Do NOT modify code between this footer and the header above #######
@@ -101,4 +82,4 @@ POT,
  * Public Functions     *
  ************************/
 
-#endif /* _GPIO_CFG_H_ */
+#endif /* _ADC_CFG_H_ */
