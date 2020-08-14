c1ead664e15b5a5481709a5c97f8b364b49773c2
jmdiaz1742 <jmdiaz1742@gmail.com>
2020-08-13 19:57:16 -0600

    Project cleanup, removing warnings

--- Code_generator/src/gui/ProjectSettingsWindow.java
+++ Code_generator/src/gui/ProjectSettingsWindow.java
@@ -9,18 +9,13 @@ import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JTextField;
 import java.awt.Insets;
 import javax.swing.JButton;
-import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class ProjectSettingsWindow {
 
